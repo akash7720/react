26456d84848cf7bc72aa1304d9f794e6e2c97b1e
akashsonune <akashsonune7720@gmail.com>
2024-03-19 16:23:18 +0530

    updates

--- frontend/src/Components/23-12/Todos.jsx
+++ frontend/src/Components/23-12/Todos.jsx
@@ -1,19 +1,37 @@
 
-import React from 'react'
+// import React from 'react'
+// import toast from 'react-hot-toast'
+
+// const Todos = ({AddTodo,todos}) => {
+//    toast.success("Hi from todos..")
+//   return (
+//     <div>
+//         <h1>Todos</h1>
+//         {todos.map((todo)=>(
+//              <h2>{todo}</h2>
+//         ))}
+//        <button onClick={AddTodo}>Add todo</button>
+       
+//     </div>
+//   )
+// }
+
+// export default Todos
+
+import React, { memo } from 'react'
 import toast from 'react-hot-toast'
 
-const Todos = ({AddTodo,todos}) => {
-   toast.success("Hi from todos..")
-  return (
-    <div>
-        <h1>Todos</h1>
-        {todos.map((todo)=>(
-             <h2>{todo}</h2>
-        ))}
-       <button onClick={AddTodo}>Add todo</button>
-       
-    </div>
-  )
+const Todos = ({ AddTodo, todos }) => {
+    toast.success("Hi from todos..")
+    return (
+        <div>
+            <h1>Todos</h1>
+            {todos.map((todo) => (
+                <h2>{todo}</h2>
+            ))}
+            <button onClick={AddTodo}> Add todo</button>
+        </div>
+    )
 }
 
-export default Todos
+export default memo(Todos);
